e4e8d06ee5c894e9c0af5c17547f59d06ad7b082
Naughie <zomacchi@gmail.com>
2018-09-23 20:57:17 +0900

    Take chapter number as ARGV in script/convert.rb

--- script/convert.rb
+++ script/convert.rb
@@ -2,7 +2,9 @@ require 'bundler/setup'
 Bundler.require
 
 TARGETDIR = Pathname.new('./img').freeze
-FILENAME = Pathname.new("./docs/maclane/categories_for_the_working_mathematicians.pdf").freeze
+SOURCEDIR = Pathname.new('./docs/maclane').freeze
+
+filename = SOURCEDIR / "#{ARGV[0]}.pdf"
 
 DROPBOX_URL = 'https://content.dropboxapi.com'.freeze
 UPLOAD_PATH = '/2/files/upload'.freeze
@@ -13,12 +15,12 @@ FileUtils.mkdir_p(TARGETDIR)
 puts 'Converting PDF into images...'
 
 begin
-  images = Magick::Image.read(FILENAME) do
+  images = Magick::Image.read(filename) do
     self.format = 'PDF'
     self.quality = 100
     self.density = 192
   end.each_with_index do |image, i|
-    image_name = TARGETDIR / "#{File.basename(FILENAME, '.pdf')}_#{i + 1}.png"
+    image_name = TARGETDIR / "#{File.basename(filename, '.pdf')}_#{i + 1}.png"
     image.write image_name
     puts "Created #{image_name}!"
   end
@@ -38,8 +40,8 @@ begin
               File.read(TARGETDIR / f),
               authorization: "Bearer #{ENV['DROPBOX_ACCESS_TOKEN']}",
               content_type: 'application/octet-stream',
-              dropbox_api_arg: %({"path": "#{DROPBOX_DIR / f}", "mode": "overwrite", "autorename": false, "mute": true})
-    puts "Uploaded #{TARGETDIR / f}!"
+              dropbox_api_arg: %({"path": "#{DROPBOX_DIR / "maclane_chapter#{f}"}", "mode": "overwrite", "autorename": false, "mute": true})
+    puts "Uploaded #{TARGETDIR / "maclane_chapter#{f}"}!"
   end
 
   puts 'Finished uploading images.'
